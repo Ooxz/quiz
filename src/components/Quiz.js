4af8b711a81921f68b1880ca0099026d63c98058
Brune <abrune.osteo@gmail.com>
2022-06-04 22:10:15 +0200

    fetch

--- src/components/Quiz.js
+++ src/components/Quiz.js
@@ -1,3 +1,25 @@
+import React, { useState, useEffect } from 'react';
+
 export default function Quiz() {
-	<div>Hello World</div>
+    const [questions, setQuestions] = useState({});
+
+	function getQuestions() {
+		fetch('https://opentdb.com/api.php?amount=10&category=9&difficulty=easy&type=multiple')
+			.then(res => res.json())
+			.then(data => setQuestions(data.results))
+			.catch(err => console.log(err));
+	}
+
+
+	useEffect(() => {
+		getQuestions()
+		}, []);
+
+		console.log(questions)
+
+
+
+	return (
+		<div>lol</div>
+	)
 }
